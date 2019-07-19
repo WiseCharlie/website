57af28445af1b6e4cdaeaaba9771f30d55eb55d1
MIHIR PATEL <mihirptl89@gmail.com>
2019-07-19 17:53:53 -0500

    remove swipebox

--- js/main.js
+++ js/main.js
@@ -2,17 +2,6 @@
 * NAVIGATION
 */
 'use strict';
-
-/*
-* SWIPEBOX
-*/
-$(document).ready(function() {
-    /* Basic Gallery */
-    $('.swipebox').swipebox();
-    /* Video */
-    $('.swipebox-video').swipebox();
-});
-
 /*
 * DROPDOWN
 */
@@ -25,80 +14,6 @@ $(document).on('change', '.div-toggle', function() {
 $(document).ready(function() {
     $('.div-toggle').trigger('change');
 });
-
-// /*
-// * SHOP
-// */
-// var App = (function() {
-
-//     //=== Private Variables ===//
-//     var gallery = $('#js-gallery');
-
-//     //=== Gallery Object ===//
-//     var Gallery = {
-//         zoom: function(imgContainer, img) {
-//             var containerHeight = imgContainer.outerHeight(),
-//                 src = img.attr('src');
-//         },
-//         switch: function(trigger, imgContainer) {
-//             var src = trigger.attr('href'),
-//                 thumbs = trigger.siblings(),
-//                 img = trigger.parent().prev().children();
-
-//             // Add active class to thumb
-//             trigger.addClass('is-active');
-
-//             // Remove active class from thumbs
-//             thumbs.each(function() {
-//                 if ($(this).hasClass('is-active')) {
-//                     $(this).removeClass('is-active');
-//                 }
-//             });
-
-//             // Reset container if in zoom state
-//             if (gallery.hasClass('is-zoomed')) {
-//                 gallery.removeClass('is-zoomed');
-//                 imgContainer.css("height", "auto");
-//             }
-
-//             // Switch image source
-//             img.attr('src', src);
-//         }
-//     };
-
-//     //=== Public Methods ===//
-//     function init() {
-
-//         // Listen for clicks on anchors within gallery
-//         gallery.delegate('a', 'click', function(event) {
-//             var trigger = $(this);
-//             var triggerData = trigger.data("gallery");
-
-//             if (triggerData === 'zoom') {
-//                 var imgContainer = trigger.parent(),
-//                     img = trigger.siblings();
-//                 Gallery.zoom(imgContainer, img);
-//             } else if (triggerData === 'thumb') {
-//                 var imgContainer = trigger.parent().siblings();
-//                 Gallery.switch(trigger, imgContainer);
-//             } else {
-//                 return;
-//             }
-
-//             event.preventDefault();
-//         });
-
-//     }
-
-//     //=== Make Methods Public ===//
-//     return {
-//         init: init
-//     };
-
-// })();
-
-// App.init();
-
 
 /*
 * AVATAR CHANGE
